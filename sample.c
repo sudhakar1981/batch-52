9054b010f3b5c44ea4591eefe314ef096da788f9
sudhakar <trainer@gmail.com>
2020-06-23 09:19:49 +0530

    task - 34785 completed (multiplication code implemented)

--- sample.c
+++ sample.c
@@ -3,14 +3,17 @@
 
 main()
 {
-	int a, b,c,d,e;
+	int a, b,c,d,e,f;
 	
 	a=30;
 	b=20;
 	c=a+b;
 	e=a-b;
+	f=a*b;
 	
 	printf("the addition of two number is = %d",c);
 	printf("the substraction of two number is = %d",e);
+	printf("the multiplication of two numbers is = %d",f);
+	
 getchc();
 }
