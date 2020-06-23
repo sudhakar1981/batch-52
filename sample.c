62ad33ec75c1123a2309268476d591a2d44e804f
sudhakar <trainer@gmail.com>
2020-06-23 09:22:19 +0530

    task - 7658 (completed division code)

--- sample.c
+++ sample.c
@@ -3,14 +3,16 @@
 
 main()
 {
-	int a, b,c,d,e;
+	int a, b,c,d,e,g;
 	
 	a=30;
 	b=20;
 	c=a+b;
 	e=a-b;
+	g=a/b;
 	
 	printf("the addition of two number is = %d",c);
 	printf("the substraction of two number is = %d",e);
+	printf("the division of two numbes is = %d",g);
 getchc();
 }
